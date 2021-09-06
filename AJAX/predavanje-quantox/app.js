f59c9dcc373093d192d9c8fc40b9b5c144ea6097
vukstojanovic <vuks838@gmail.com>
2021-09-06 17:00:38 +0200

    sdgfh

--- AJAX/predavanje-quantox/app.js
+++ AJAX/predavanje-quantox/app.js
@@ -1,24 +1,41 @@
 
+// $(".btn").click(function () {
+
+//     $(".text").text("...loading");
+
+//     $.ajax({
+//         type: "GET",
+//         url: "https://learnwebcode.github.io/json-example/animals-1.json",
+//         success: function(data) {
+//             $(".text").text(JSON.stringify(data));
+//         },
+//         datatype: "jsonp"
+//     });
+
+// });
+
+
 $(".btn").click(function () {
 
     $(".text").text("...loading");
 
     $.ajax({
         type: "GET",
-        url: "https://dog.ceo/api/breeds/list/all",
-        success: function(data) {
-            $(".text").text(data);
+        data: {
+            q: "dog",
+            restrict_sr: true
         },
-        dataType: "jsonp"
+        url: "https://www.reddit.com/r/aww/search.json",
+        success: function(result) {
+            $(".text").text("");
+            result.data.children.forEach(item => {
+                $(".text").append(`<img src="${item.data.thumbnail}" alt=""/>`);
+            });
+        },
+        datatype: "jsonp"
     });
 
 });
-
-
-
-
-
-
 
 
 
