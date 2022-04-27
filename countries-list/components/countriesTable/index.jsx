5f6a2e1c129a3f019c950f12ecbb7492bf193eec
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2022-04-27 18:00:46 +0200

    Implement getStaticProps

--- countries-list/components/countriesTable/index.jsx
+++ countries-list/components/countriesTable/index.jsx
@@ -1,25 +1,76 @@
 import styles from "./countriesTable.module.css";
 import countries from "../dummy";
+import { FaSearch } from "react-icons/fa";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
 
-function CountriesTable() {
+function CountriesTable({ countriesList }) {
+  const router = useRouter();
+  console.log(countriesList);
+
+  function searchByInput(e) {
+    e.preventDefault();
+    router.push({ query: { sea: "someLetter" } });
+  }
+
   return (
-    <section className={styles["countries-table"]}>
-      <div className={styles["head-field"]}>Name</div>
-      <div className={styles["head-field"]}>Population</div>
-      <div className={styles["head-field"]}>Area (km)</div>
-      <div className={styles["head-field"]}>Gini</div>
-      {countries.map((country) => {
-        const { name, population, area, gini } = country;
-        return (
-          <>
-            <div className={styles["country-field"]}>{name}</div>
-            <div className={styles["country-field"]}>{population}</div>
-            <div className={styles["country-field"]}>{area}</div>
-            <div className={styles["country-field"]}>{gini}</div>
-          </>
-        );
-      })}
-    </section>
+    <div className={styles["countries-page"]}>
+      <section className={styles["search-segment"]}>
+        <p>Found 3 countries</p>
+        <form onSubmit={searchByInput} className={styles["input-container"]}>
+          <FaSearch size={18} />
+          <input type="text" placeholder="Filter by Name, Region, Subregion" />
+        </form>
+      </section>
+      <section className={styles["countries-table"]}>
+        <div className={styles["head-field"]}>&nbsp;</div>
+        <div className={`${styles["head-field"]} ${styles["head-name"]}`}>
+          Name
+        </div>
+        <div className={styles["head-field"]}>Population</div>
+        <div className={styles["head-field"]}>Area (km)</div>
+        <div className={styles["head-field"]}>Gini</div>
+        {countriesList.map((country, index) => {
+          const { flags, name, population, area, gini } = country;
+          const flag = (flags && flags.png) || (flags && flags.svg);
+          const commonName = name.common;
+          const giniData = gini && Object.values(gini)[0];
+          return (
+            <Fragment key={index}>
+              <div className={styles["country-field"]}>
+                <img
+                  className={styles["country-flag"]}
+                  src={flag}
+                  alt={`flag of ${commonName}`}
+                />
+              </div>
+              <div
+                className={`${styles["country-field"]} ${styles["country-name"]}`}
+              >
+                <span>{commonName}</span>
+              </div>
+              <div className={styles["country-field"]}>
+                {population.toLocaleString()}
+              </div>
+              <div className={styles["country-field"]}>
+                {area.toLocaleString()}
+              </div>
+              <div className={styles["country-field"]}>
+                <div className={styles["bar-container"]}>
+                  <div
+                    className={styles["bar-filler"]}
+                    style={{ width: `${giniData ? giniData : 0}%` }}
+                  ></div>
+                </div>
+                <span className={styles["gini-text"]}>
+                  {giniData ? `${giniData}%` : "N/A"}
+                </span>
+              </div>
+            </Fragment>
+          );
+        })}
+      </section>
+    </div>
   );
 }
 
--- countries-list/components/dummy/index.js
+++ countries-list/components/dummy/index.js
@@ -1,21 +1,24 @@
 const countries = [
   {
-    name: "China",
+    name: "France",
     population: 1300,
     area: 1240,
-    gini: "49%",
+    gini: 49,
+    flag: "https://flagcdn.com/w320/fr.png",
   },
   {
-    name: "India",
+    name: "Germany",
     population: 1400,
     area: 3456,
-    gini: "34%",
+    gini: 34,
+    flag: "https://flagcdn.com/w320/de.png",
   },
   {
-    name: "USA",
+    name: "Finland",
     population: 1000,
     area: 2232,
-    gini: "11%",
+    gini: 62,
+    flag: "https://flagcdn.com/w320/fi.png",
   },
 ];
 
