4eccc8a85c6a217e159091c4e39e409efa367d04
vukstojanovic <vuks838@gmail.com>
2022-05-10 18:07:49 +0200

    Add chatroom

---
+++ chat-app/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import { ChatEngine } from "react-chat-engine";
+
+function App() {
+  return <ChatEngine height="100vh" projectID="" userName="" userSecret="" />;
+}
+
+export default App;
