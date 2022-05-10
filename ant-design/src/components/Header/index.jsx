351247589d2e1fdb5ffb8ca1ebaca5d13a187bd5
vukstojanovic <vuks838@gmail.com>
2022-05-10 14:38:22 +0200

    Add antd

---
+++ ant-design/src/components/Header/index.jsx
@@ -0,0 +1,24 @@
+import { Layout, Menu } from "antd";
+const { Header } = Layout;
+
+function Header() {
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={new Array(3).fill(null).map((_, index) => {
+            const key = index + 1;
+            return {
+              key,
+              label: `nav ${key}`,
+            };
+          })}
+        />
+      </Header>
+    </Layout>
+  );
+}
