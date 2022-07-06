7100d99fe48a4b4bc38fdf75ed656fe238f33bdb
vukstojanovic <vuks838@gmail.com>
2022-07-06 17:32:32 +0200

    Experiment with useRoutes hook

--- react-router-6/src/App.jsx
+++ react-router-6/src/App.jsx
@@ -6,39 +6,71 @@ import Employees from "./components/Employees";
 import ProjectsPage from "./components/ProjectsPage";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import { Routes, Route, Navigate, useRoutes, Outlet } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
-  const routeElement = useRoutes([
+  const [season, setSeason] = useState("duck");
+  const duckRoutes = [
+    {
+      path: "/ducks",
+      element: <div>Qua qua qua qua qua qua!!!!</div>,
+    },
+    {
+      path: "/duffy",
+      element: (
+        <div>
+          <b>Boom chaka boom chaka boom chaka boom</b>
+        </div>
+      ),
+    },
     {
       path: "/",
-      element: <div>Welcome!!!!</div>,
-    },
-    {
-      path: "/home",
       element: (
         <div>
-          <b>Home</b>
-          <Outlet />
-        </div>
-      ),
-      children: [
-        { path: "one", element: <div>One</div> },
-        { path: "two", element: <div>Two</div> },
-      ],
-    },
-    {
-      path: "/about",
-      element: (
-        <div>
-          <b>About</b>
+          <b>check out duck routes</b>
         </div>
       ),
     },
-  ]);
+  ];
+  const rabbitRoutes = [
+    {
+      path: "/rabbits",
+      element: <div>Hop hop hop hop hop hop hop!!!</div>,
+    },
+    {
+      path: "/bugs",
+      element: (
+        <div>
+          <b>This means war</b>
+        </div>
+      ),
+    },
+    {
+      path: "/",
+      element: (
+        <div>
+          <b>check out rabbit routes</b>
+        </div>
+      ),
+    },
+  ];
+  const currentRoutes = season === "duck" ? duckRoutes : rabbitRoutes;
+  const routeElement = useRoutes(currentRoutes);
+
+  function changeSeason() {
+    if (season === "duck") {
+      setSeason("rabbit");
+    } else {
+      setSeason("duck");
+    }
+  }
 
   return (
     <div className="App">
       {/* <MainHeader /> */}
+      <header>
+        <button onClick={changeSeason}>{season} season!!!</button>
+      </header>
       <main>
         {routeElement}
         {/* <Routes>
