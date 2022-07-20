dd14f344ff92da720325ef177524c779a0b52418
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2022-07-20 21:33:15 +0200

    Add react query with next

--- next-eom/pages/_app.jsx
+++ next-eom/pages/_app.jsx
@@ -1,11 +1,22 @@
 import "../styles/globals.css";
 import Layout from "../components/layout/index";
+import {
+  QueryClient,
+  QueryClientProvider,
+  Hydrate,
+} from "@tanstack/react-query";
 
 function MyApp({ Component, pageProps }) {
+  const newClient = new QueryClient();
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={newClient}>
+      {/* <Hydrate state={pageProps.dehydratedState}> */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      {/* </Hydrate> */}
+    </QueryClientProvider>
   );
 }
 
--- next-eom/pages/index.jsx
+++ next-eom/pages/index.jsx
@@ -1,12 +1,49 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Card from "../components/card/Card";
+import axios from "axios";
+import { useQuery, QueryClient, dehydrate } from "@tanstack/react-query";
+import { useEffect } from "react";
+
+async function getUsers() {
+  return axios.get("http://localhost:2000/users");
+}
 
 export default function Home() {
-  return (
-    <div className={styles["home-page"]}>
-      <h1>Next.js news app</h1>
-      <h3>You're one stop shop from the latest news articles</h3>
-    </div>
+  const { data, isLoading, isSuccess, isError, error } = useQuery(
+    ["users"],
+    getUsers
   );
+
+  useEffect(() => {
+    console.log(isSuccess);
+    console.log(error);
+  }, [isLoading]);
+
+  if (isLoading) return <h1>Loading...</h1>;
+
+  if (isSuccess) {
+    return (
+      <div className={styles["home-page"]}>
+        <h1>Next.js news app</h1>
+        <h3>You're one stop shop from the latest news articles</h3>
+        <div className="container">
+          {data.data?.map((user, index) => (
+            <Card key={`user-${index}`} {...user} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
+
+// export async function getStaticProps() {
+//   const queryClient = new QueryClient();
+
+//   await queryClient.prefetchQuery(["users"], getUsers);
+
+//   return {
+//     props: {
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   };
+// }
