202513192016dda0a81549210efbb90e0fe1d84e
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2022-07-23 18:02:52 +0200

    add eslint

---
+++ eslint/src/App.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          <code>src/App.tsx</code>
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit illo non veniam quis
+          aspernatur voluptas ipsam perferendis. Laboriosam possimus quae debitis nam eveniet
+          voluptate facere laborum quis ad. Recusandae, provident. Nobis tenetur, beatae, molestiae
+          odit obcaecati voluptas consequatur adipisci dolore ipsa tempore distinctio saepe
+          repellendus, perferendis eligendi quaerat? Amet accusamus nostrum sapiente. Deleniti
+          impedit harum omnis voluptatibus eius dolorum accusantium! Quam temporibus facilis totam
+          eaque saepe odio reiciendis nam unde dolor vero earum vel, architecto animi optio
+          consequuntur deserunt ex iste quos, cupiditate numquam est veniam assumenda? Quisquam,
+          deserunt neque. Asperiores ipsam officia natus aliquid autem vero tempore iste illum!
+          Velit saepe corrupti ex ut possimus eligendi aliquam hic, dolores, fugiat error commodi
+          accusantium porro sint distinctio odio incidunt corporis. Inventore in distinctio ab
+          aliquam deleniti officiis dolores iure quibusdam! Impedit quo quisquam officiis animi
+          explicabo, odio cumque necessitatibus esse, perferendis culpa unde nam provident?
+          Voluptatibus minima praesentium accusamus harum.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
