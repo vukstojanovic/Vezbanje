e72283405d4d5aeae859f0593e2709f0350e1bed
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2022-09-07 19:45:00 +0200

    Delete typescript folder

--- typescript/src/components/SingleChild.tsx
+++
@@ -1,6 +0,0 @@
-import { useAppContext } from "../context";
-
-export default function SingleChild() {
-  const { user, setUser } = useAppContext();
-  return <div>This is a single child component. User: {String(user)}</div>;
-}
